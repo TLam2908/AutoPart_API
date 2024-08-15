0595b75f71e25d7696f84581a460931b9a77ab09
TLam2908 <buitunglam2003@gmail.com>
2024-08-15 10:58:15 +0700

    Update database.js

--- services/database.js
+++ services/database.js
@@ -9,7 +9,7 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     max: 20, // số lượng kết nối tối đa trong pool
     idleTimeoutMillis: 30000, // thời gian chờ kết nối không sử dụng trước khi bị đóng
-    connectionTimeoutMillis: 2000, // thời gian chờ kết nối trước khi báo lỗi
+    connectionTimeoutMillis: 10000, // thời gian chờ kết nối trước khi báo lỗi
 })
 
 pool.on('error', (err, client) => {
