ccb74d439e600d1c9dff8052a388ac9d173c5125
TLam2908 <buitunglam2003@gmail.com>
2024-08-31 17:18:01 +0700

    Update database.js

--- services/database.js
+++ services/database.js
@@ -10,6 +10,7 @@ const pool = new Pool({
     max: 20, // số lượng kết nối tối đa trong pool
     idleTimeoutMillis: 30000, // thời gian chờ kết nối không sử dụng trước khi bị đóng
     connectionTimeoutMillis: 10000, // thời gian chờ kết nối trước khi báo lỗi
+    search_path: 'autopart'
 })
 
 pool.on('error', (err, client) => {
